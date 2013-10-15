028c20395e7576f4f6d3c3e67d6fc9f7db14b884
Katie Roberts <krobertscreative@gmail.com>
2013-10-15 13:37:05 -0400

    Added responsiveness to images, styled API data display, began filtering functionality

--- js/spark-app/controllers/mainController.js
+++ js/spark-app/controllers/mainController.js
@@ -1,28 +1,44 @@
 app.controller("mainController", function($scope, $http){
 	
 	$scope.apiKey = "WfS8an7aqWaOjYzGnPIfCAJkMi1wd0Us";
+	//array to hold processed results
 	$scope.results = [];
+	$scope.filterText = null;
+	$scope.availableFields = [];
+    $scope.fieldFilter = null;
+    //takes field value and sets $scope.fieldFilter to the value
+    $scope.setFieldFilter = function(field) {
+    	$scope.fieldFilter = field;
+	}
     $scope.init = function() {
         
-        /* BEHANCE */
+        /* BEHANCE api call *//**/
         $http.jsonp('http://behance.net/v2/projects?api_key=' + $scope.apiKey + '&callback=JSON_CALLBACK')
         .success(function(data) {
-        
-            console.log(data);
-			/**/	 
-			
-			var name = data.projects[0].name;
-			console.log(name);
-			
-            //For each project, get all the project data
-            angular.forEach(data, function(projects, index){
-            	
-            	var name = projects.name;
-            	console.log(name);
+        	
+        	console.log(data.projects); //============================
+        	
+			//Loop through each projects, push it to the results array
+			angular.forEach(data.projects, function(projects, index){
+                
                 $scope.results.push(projects);
+				console.log($scope.results); //returns array of objects (projects)
                 
-                
+                //Loop through each field for this project
+                angular.forEach(projects.field, function(field, index){
+                    //Only add to the availableFields array if it doesn't already exist
+                    var exists = false;
+                    angular.forEach($scope.availableFields, function(avField, index){
+                        if (avField == field) {
+                            exists = true;
+                        }
+                    });
+                    if (exists === false) {
+                        $scope.availableFields.push(field);
+                    }
+                });
             });
+			
 		})
         .error(function(error) {
  
@@ -30,5 +46,23 @@ app.controller("mainController", function($scope, $http){
        
     };
 
+});
+//custom filter: input provided by default and is the data that ng-repeat is processing, field is a value to pass in
+app.filter('isField', function() {
+    return function(input, field) {
+        if (typeof field == 'undefined' || field == null) {
+            return input;
+        } else {
+            var out = [];
+            for (var a = 0; a < input.length; a++){
+                for (var b = 0; b < input[a].projects.fields.length; b++){
+                    if(input[a].projects.fields[b] == field) {
+                        out.push(input[a]);
+                    }
+                }
+            }
+            return out;
+        }
+    };
 });
 
